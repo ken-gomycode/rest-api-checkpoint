35462087e0079dec7b4090149b8696571aa6bb36
Ken Ovienadu <kenovienadu@gmail.com>
2025-02-27 11:15:49 +0100

    feat(server): add global error handler middleware(main)

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, {NextFunction, Request, Response} from "express";
 import handlers from "./handlers";
 
 const server = express();
@@ -12,6 +12,18 @@ server.post('/users', handlers.addUser);
 server.get('/users/:id', handlers.getUserById);
 server.put('/users/:id', handlers.updateUser);
 server.delete('/users/:id', handlers.deleteUser);
+
+
+// Global Error Handler Middleware
+server.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack); // Log error for debugging
+
+  res.status(err.status || 500).json({
+    title: "Error",
+    message: err?.message || "Something went wrong!",
+    status: err?.status || 500,
+  })
+});
 
 // Handle server errors
 server.on("error", (error: any) => {
